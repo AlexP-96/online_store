bc154609064447f24fc2fe23eaf7b1411d8ebf47
AlexPupyshev <lubu221996@gmail.com>
2024-02-21 11:41:46 +0500

    начато выполнение формы регистрации пользователя

--- src/authorization/Registration.jsx
+++ src/authorization/Registration.jsx
@@ -1,77 +1,101 @@
 import React, { useState } from 'react';
 
 import './style.css';
-import { useSelector, useDispatch } from 'react-redux';
+import {
+    useSelector,
+    useDispatch,
+} from 'react-redux';
+import spinner from '../source/spinners/Iphone-spinner-2.gif'
 import rootingReducer from '../reducers/authReduser';
 
 const Authorization = () => {
 
     const url = 'http://localhost:3000/';
 
-    const [registration, setRegistration] = useState({ name: '', lastName: '', email: '', password: '', secret: '' });
-    const [isValid, setIsValid] = useState(true);
+    const [registration, setRegistration] = useState({
+        name: '',
+        lastName: '',
+        email: '',
+        password: '',
+        secret: '',
+    });
+    const [isLoad, setLoad] = useState(false);
 
     const validateEmail = (email) => {
         const regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
         return regex.test(email);
     };
 
-
     const regName = (e) => {
         setRegistration({
             ...registration,
-            name: e.target.value
-        })
-    }
+            name: e.target.value,
+        });
+    };
     const regLastName = (e) => {
         setRegistration({
             ...registration,
-            lastName: e.target.value
-        })
-    }
+            lastName: e.target.value,
+        });
+    };
     const regEmail = (e) => {
         setRegistration({
             ...registration,
-            email: e.target.value
-        })
-    }
+            email: e.target.value,
+        });
+    };
     const setPassword = (e) => {
         setRegistration({
             ...registration,
-            password: e.target.value
-        })
-    }
+            password: e.target.value,
+        });
+    };
 
     const submitData = (e) => {
-
+        setLoad(true);
         e.preventDefault();
 
         if (validateEmail(registration.email)) {
-            setIsValid(true);
-            // Код для отправки данных на сервер
-            const response = fetch(url + 'accounts/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json;charset=utf-8'
-                },
-                body: JSON.stringify(registration)
-            }).then(res => console.log(res))
-            setRegistration({
-                name: '',
-                lastName: '',
-                email: '',
-                password: ''
-            })
 
-        } else {
-            setIsValid(false);
-            alert('email введен неккорктно')
+            (async function () {
+                try {
+
+                    let response = await fetch(url + 'accounts');
+
+                    let fetchUsers = await response.json();
+
+                    let dataCurrent = fetchUsers.find(empty => (registration.email === empty.email));
+
+                    if (dataCurrent === undefined) {
+                        let postData = await fetch(url + 'accounts', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json;charset=utf-8',
+                            },
+                            body: JSON.stringify(registration),
+                        });
+
+                        let dataRes = await postData.json();
+                        await setRegistration({
+                            name: '',
+                            lastName: '',
+                            email: '',
+                            password: '',
+                            secret: '',
+                        });
+                        setLoad(false);
+                        alert(`Пользователь с именем ${dataRes.name} изпешно зарегистрирован`)
+                    } else {
+                        alert('Данный email уже заргестрирован, попробуйде другой');
+                        setLoad(false);
+                    }
+                } catch (err) {
+                    alert(err);
+                }
+            })();
+
         }
-
-
-        
-    }
-
+    };
 
     const selectorName = useSelector(state => state.auth.name);
 
@@ -80,34 +104,52 @@ const Authorization = () => {
     return (
         <div className='c__authorization'>
             <h3>Регистрация</h3>
-            <form action="" className='form__authorization'>
-                <input type="text"
-                    required
-                    value={registration.name}
-                    placeholder='name'
-                    onChange={regName}
-                />
-                <input type="text"
-                    placeholder='last_name'
-                    value={registration.lastName}
-                    onChange={regLastName} />
-                <input type="text"
-                    required
-                    placeholder='email'
-                    value={registration.email}
-                    onChange={regEmail}
-                />
-                <input type="password"
-                    required
-                    placeholder='password'
-                    value={registration.password}
-                    onChange={setPassword}
-                />
-                <input type="hidden" value={'hidden'} />
-                <input type='submit'
-                    value={'Регистрация'}
-                    onClick={(e) => submitData(e)} />
-            </form>
+            {isLoad && <img
+                src={spinner}
+                alt='spinner'
+            />}
+            {!isLoad &&
+                <form
+                    onSubmit={submitData}
+                    action=''
+                    className='form__authorization'
+                >
+                    <input
+                        type='text'
+                        required
+                        value={registration.name}
+                        placeholder='name'
+                        onChange={regName}
+                    />
+                    <input
+                        type='text'
+                        placeholder='last_name'
+                        value={registration.lastName}
+                        onChange={regLastName}
+                    />
+                    <input
+                        type='text'
+                        required
+                        placeholder='email'
+                        value={registration.email}
+                        onChange={regEmail}
+                    />
+                    <input
+                        type='password'
+                        required
+                        placeholder='password'
+                        value={registration.password}
+                        onChange={setPassword}
+                    />
+                    <input
+                        type='hidden'
+                        value={'hidden'}
+                    />
+                    <input
+                        type='submit'
+                        value={'Регистрация'}
+                    />
+                </form>}
         </div>
     );
 };
